339c0ed1184f8be732ce90b341017619449fced3
Mark IJbema <markijbema@gmail.com>
2013-08-01 22:17:29 +0200

    cleared level 5

--- gerard-beginner/player.rb
+++ gerard-beginner/player.rb
@@ -1,27 +1,64 @@
-class Player
-  def play_turn(warrior)
-    @state ||= :walk
-    @last_health ||= warrior.health
+class Hero
+  attr_accessor :warrior, :state, :last_health
+  def initialize
+    @state = :walk
+    @last_health = 20
+  end
 
-    under_attack = @last_health > warrior.health
-    @last_health = warrior.health
+  def under_attack?
+    last_health > warrior.health
+  end
 
-    hurting = warrior.health < 20
+  def hurting?
+    warrior.health < 20
+  end
 
-    if warrior.feel.empty?
-      if hurting and not under_attack
-        @state = :rest
-      else
-        @state = :walk
-      end
+  def in_next_cell
+    cell = warrior.feel
+    if cell.empty?
+      :nothing
+    elsif cell.captive?
+      :captive
     else
-      @state = :attack
+      :enemy
     end
+  end
 
-    case @state
+  def next_state
+    case in_next_cell
+    when :nothing
+      if hurting? and not under_attack?
+        :rest
+      else
+        :walk
+      end
+    when :enemy
+      :attack
+    when :captive
+      :rescue
+    end
+  end
+
+  def make_a_move
+    new_state = next_state
+
+    case new_state
     when :walk   then warrior.walk!
     when :rest   then warrior.rest!
     when :attack then warrior.attack!
+    when :rescue then warrior.rescue!
     end
+
+    @state = next_state
+    @last_health = warrior.health
+  end
+end
+
+class Player
+  def play_turn(warrior)
+    @hero ||= Hero.new
+    @hero.warrior = warrior
+
+    @hero.make_a_move
   end
 end
